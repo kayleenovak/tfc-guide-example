bdcbc20f877480d80007f35a110d7f75e48a0fd9
Kaylee Bunce <kaybunce@hotmail.com>
2022-04-04 16:17:50 -0600

    Change file structure

--- modules/lambda_zip/lambda_zip.tf
+++ modules/lambda_zip/lambda_zip.tf
@@ -1,5 +1,5 @@
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = "./lambdas/${var.source_dir_path}"
-  output_path = "./lambdas/${var.zip_filename}"
+  output_path = "./nodejs/node_modules/${var.zip_filename}"
 }
