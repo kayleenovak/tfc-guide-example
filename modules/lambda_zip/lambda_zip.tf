b890513a07232e35f82db035c170d3e0af024a8e
Kaylee Bunce <kaybunce@hotmail.com>
2022-04-04 17:01:05 -0600

    get your head in the game

--- modules/lambda_zip/lambda_zip.tf
+++ modules/lambda_zip/lambda_zip.tf
@@ -1,5 +1,5 @@
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir  = "../lambdas/zipThis"
+  source_dir  = "./lambdas/zipThis"
   output_path = "./lambdas/${var.zip_filename}"
 }
