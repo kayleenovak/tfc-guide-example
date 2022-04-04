94c5895c12e40b401b50aa720fad0da8c62ff792
Kaylee Bunce <kaybunce@hotmail.com>
2022-04-04 15:48:24 -0600

    Update path

--- modules/lambda_zip/lambda_zip.tf
+++ modules/lambda_zip/lambda_zip.tf
@@ -1,5 +1,5 @@
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir  = "./canary/${var.source_dir_path}"
-  output_path = "./canary/${var.zip_filename}"
+  source_dir  = "./lambdas/${var.source_dir_path}"
+  output_path = "./lambdas/${var.zip_filename}"
 }
