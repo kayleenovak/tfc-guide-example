a0cd57031e301992512fa42dcba12e990f994eb9
Kaylee Bunce <kaybunce@hotmail.com>
2022-04-04 15:27:59 -0600

    replace tomap

--- modules/lambda_zip/outputs.tf
+++ modules/lambda_zip/outputs.tf
@@ -1,8 +1,9 @@
 output "lambda_zip" {
   value = "${
-    map(
-      "output_path", "${data.archive_file.lambda_zip.output_path}",
-      "output_base64sha256" , "${data.archive_file.lambda_zip.output_base64sha256}"
+    tomap({
+      "output_path" = "${data.archive_file.lambda_zip.output_path}",
+      "output_base64sha256" = "${data.archive_file.lambda_zip.output_base64sha256}"
+    }
     )
   }"
 }
